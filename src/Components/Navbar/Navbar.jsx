d4da32e0f69d9aa04fd4c7a74b8f8ce4c76c0a83
Arif Dhali <arifdhali98@gmail.com>
2023-12-21 22:56:10 +0530

    Navbar Complete

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
+import "./Navbar.css";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+import logo from "../../assets/pagebolt-logo.webp";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="navbar container">
+      <a href="" className="navbar-logo">
+        <figure>
+          <img src={logo} alt="Logo" />
+        </figure>
+      </a>
+      <ul className="navbar-links">
+        <li>
+          <a href="#home">Home</a>
+        </li>
+        <li>
+          <a href="#product">Product</a>
+        </li>
+        <li>
+          <a href="#solutions">Solutions</a>
+        </li>
+        <li>
+          <a href="#security">Security</a>
+        </li>
+        <li>
+          <a href="#features">Features</a>
+        </li>
+      </ul>
 
-export default Navbar
+      <div className="btn">
+        <a href="" className="btn ">
+          Login
+        </a>
+        <a href="" className="btn btn-primary">
+          Sign up
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
