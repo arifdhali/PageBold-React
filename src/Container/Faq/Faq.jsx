fa110d329038ef16415f1a6fd43ae36794027726
Arif Dhali <arifdhali98@gmail.com>
2023-12-30 22:51:07 +0530

    faq

--- src/Container/Faq/Faq.jsx
+++ src/Container/Faq/Faq.jsx
@@ -5,10 +5,11 @@ const Faq = () => {
     <div className="container padding_t padding_b">
       <h2 className="text-center">Frequently Asked Questions</h2>
 
-      <div className="faq_content padding_t">
+      <div className="faq_container padding_t">
         <div className="faq_item">
           <div className="faq-header">
-            <h4>Are there any recurring fees?</h4>
+            <h4>Are there any recurring fees?</h4>{" "}
+            <i class="fa-solid fa-plus"></i>
           </div>
           <div className="faq-content">
             <p>
@@ -17,6 +18,44 @@ const Faq = () => {
               initial six months of support has expired – however, this is
               entirely optional. There are no recurring fees to continue using
               our themes.
+            </p>
+          </div>
+        </div>
+        <div className="faq_item">
+          <div className="faq-header">
+            <h4>How many plugins do I need to install to use a theme?</h4>{" "}
+            <i class="fa-solid fa-plus"></i>
+          </div>
+          <div className="faq-content">
+            <p>
+              Our themes only need three plugins: UiCore Framework (theme core
+              functionalities), Elementor (free page builder), and Element Pack
+              Pro (premium plugin, included for free).
+            </p>
+          </div>
+        </div>
+        <div className="faq_item">
+          <div className="faq-header">
+            <h4>Do I need to register any bundled plugin?</h4>{" "}
+            <i class="fa-solid fa-plus"></i>
+          </div>
+          <div className="faq-content">
+            <p>
+              No. All plugins are fully-featured and included for free with your
+              purchase.
+            </p>
+          </div>
+        </div>
+        <div className="faq_item">
+          <div className="faq-header">
+            <h4>How can I get support after purchase?</h4>{" "}
+            <i class="fa-solid fa-plus"></i>
+          </div>
+          <div className="faq-content">
+            <p>
+              We have a dedicated support forum with a team of agents to assist
+              you. We also recommend checking through the documentation to find
+              answers to common questions.
             </p>
           </div>
         </div>
--- src/Container/index.js
+++ src/Container/index.js
@@ -1,5 +1,5 @@
 export { default as Header } from "./Header/Header";
 export { default as Footer } from "./Footer/Footer";
-export { default as Members } from "./Members/Members";
+export { default as Members } from "./Ads/Ads";
 export { default as Faq } from "./Faq/Faq";
 export { default as Ready_made } from "./Ready_made/Ready_made";
