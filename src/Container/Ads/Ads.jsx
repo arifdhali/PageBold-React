373b6aa3b983e484a71d81ac4b1adec5c477ea27
Arif Dhali <arifdhali98@gmail.com>
2024-01-01 00:31:11 +0530

    ads

--- src/Container/Ads/Ads.jsx
+++ src/Container/Ads/Ads.jsx
@@ -2,18 +2,28 @@ import React from "react";
 import "./Ads.css";
 import banner from "../../assets/ads.webp";
 const Ads = () => {
+  const adsBanner = {
+    backgroundColor: ` var(--color-font2)`,
+    backgroundImage: `url(${banner})`,
+    backgroundRepeat: ` no-repeat`,
+    backgroundPosition: `center right`,
+    backgroundSize: `75% auto`,
+  };
   return (
-    <div className="ads padding_lr" style={`background:URL(${banner})`}>
-      <div className="ads-content padding_b padding_t">
-        <h2>
-          Take your website <br /> to the next level!
-        </h2>
-        <div className="btn-area">
-          <button className="btn-glow">Purchase on ThemeForest</button>{" "}
-          <p>or</p>
-          <button className="btn btn-primary">
-            <span>Try it for free</span>
-          </button>
+    <div className="padding_lr padding_t padding_b">
+      <div className="ads" style={adsBanner}>
+        <div className="ads-content padding_b padding_t">
+          <h2>
+            Take your website <br /> to the next level!
+          </h2>
+          <div className="btn-area">
+            <button className="btn-glow">Purchase on ThemeForest</button>{" "}
+            <p>or</p>
+            <button>
+              <span>Try it for free</span>
+            </button>
+          </div>
+          <small>One-time purchase. No additional fees or subscriptions.</small>
         </div>
       </div>
     </div>
