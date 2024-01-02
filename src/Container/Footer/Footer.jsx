c8236c2688f1658d01f005e113e1b3abe463083a
Arif Dhali <arifdhali98@gmail.com>
2024-01-02 22:29:40 +0530

    footer compelte

--- src/Container/Footer/Footer.jsx
+++ src/Container/Footer/Footer.jsx
@@ -1,9 +1,80 @@
-import React from 'react'
-
+import React from "react";
+import Logo from "../../assets/pagebolt-logo.webp";
+import "./Footer.css";
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer>
+      <div className="container padding_t">
+        <div className="row ">
+          <div className="col-md-4 ftr-logo">
+            <a href="/">
+              <img src={Logo} alt="Footer logo" />
+            </a>
+          </div>
+          <div className="col-md-3">
+            <h5>Resources</h5>
+            <ul>
+              <li>
+                <a href="">Page Builder</a>
+              </li>
+              <li>
+                <a href="">Theme Options</a>
+              </li>
+              <li>
+                <a href="">Theme Builder</a>
+              </li>
+              <li>
+                <a href="">Popup Builder</a>
+              </li>
+              <li>
+                <a href="">Animations Engine</a>
+              </li>
+            </ul>
+          </div>
+          <div className="col-md-3">
+            <h5>Resources</h5>
+            <ul>
+              <li>
+                <a href="">Page Builder</a>
+              </li>
+              <li>
+                <a href="">Theme Options</a>
+              </li>
+              <li>
+                <a href="">Theme Builder</a>
+              </li>
+              <li>
+                <a href="">Popup Builder</a>
+              </li>
+              <li>
+                <a href="">Animations Engine</a>
+              </li>
+            </ul>
+          </div>
+          <div className="col-md-2">
+            <h5>Resources</h5>
+            <ul>
+              <li>
+                <a href="">Page Builder</a>
+              </li>
+              <li>
+                <a href="">Theme Options</a>
+              </li>
+              <li>
+                <a href="">Theme Builder</a>
+              </li>
+              <li>
+                <a href="">Popup Builder</a>
+              </li>
+              <li>
+                <a href="">Animations Engine</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
